c2be1e926ec228cd6f81ce5023ceeaccab1f1479
Mateusz Myszke <myszke93@gmail.com>
2022-12-02 11:55:30 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-let computerMove = 'kamień';
 let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twoj ruch to ' + playerMove + ', to wygrywasz');
 let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove;
+
+if (randomNumber == 1) {
+    computerMove = 'kamień';
+}  else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twoj ruch to ' + playerMove + ', to wygrywasz');
+
